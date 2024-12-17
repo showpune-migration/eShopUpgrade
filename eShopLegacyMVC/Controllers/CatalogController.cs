df5878911523621452482729e4843b1c09a0144f
NoReply Amazon <no-reply@amazon.com>
2024-12-17 04:39:14 +0000

    [Commit] Iteration 21 #errors = 45 <= 46.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/eShopLegacyMVC/Controllers/CatalogController.cs][CS0246][111,40] The type or namespace name 'Exclude' could not be found (are you missing a using directive or an assembly reference?)``.

--- eShopLegacyMVC/Controllers/CatalogController.cs
+++ eShopLegacyMVC/Controllers/CatalogController.cs
@@ -108,7 +108,7 @@ namespace eShopLegacyMVC.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Exclude = "PictureUri,CatalogType,CatalogBrand")] CatalogItem catalogItem)
+        public ActionResult Edit([Bind("Id,Name,Description,Price,CatalogBrandId,CatalogTypeId,AvailableStock,RestockThreshold,MaxStockThreshold,OnReorder")] CatalogItem catalogItem)
         {
             _log.Info($"Now processing... /Catalog/Edit?id={catalogItem.Id}");
             if (ModelState.IsValid)
