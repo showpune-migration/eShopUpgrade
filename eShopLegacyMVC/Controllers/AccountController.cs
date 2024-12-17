eef3818cf67f70b3f0a89ab64f8808cec12a1e53
NoReply Amazon <no-reply@amazon.com>
2024-12-17 04:46:14 +0000

    [Commit] Iteration 35 #errors = 22 <= 23.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/eShopLegacyMVC/Controllers/AccountController.cs][CS1739][87,80] The best overload for 'SignInAsync' does not have a parameter named 'rememberBrowser'``.

--- eShopLegacyMVC/Controllers/AccountController.cs
+++ eShopLegacyMVC/Controllers/AccountController.cs
@@ -84,12 +84,12 @@ namespace eShopLegacyMVC.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+                    await SignInManager.SignInAsync(user, isPersistent: false);
 
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: HttpContext.Request.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
                     return RedirectToAction("Index", "Catalog");
