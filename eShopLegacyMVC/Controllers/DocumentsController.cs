3903be3b75fa672aa2059c30020b27cdd43af81b
NoReply Amazon <no-reply@amazon.com>
2024-12-17 04:53:48 +0000

    Revert "[Commit] Iteration 44 #errors = 17 <= 17."
    This reverts commit 7cffe47a16dabc3f44d0bb63961a2f08c905ef00.

--- eShopLegacyMVC/Controllers/DocumentsController.cs
+++ eShopLegacyMVC/Controllers/DocumentsController.cs
@@ -31,10 +31,10 @@ namespace eShopLegacyMVC.Controllers
         }
 
         [HttpPost]
-        public ActionResult UploadDocument(IFormFileCollection files)
+        public ActionResult UploadDocument()
         {
             var fileService = FileService.Create();
-            fileService.UploadFile(files);
+            fileService.UploadFile(Request.Files);
             return RedirectToAction("Index");
         }
     }
